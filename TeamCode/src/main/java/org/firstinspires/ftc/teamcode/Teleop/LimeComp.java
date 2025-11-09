b464edbb90891f2936e60162d073d9e45ac953c0
Jadzia-P <cheeky4pratt@gmail.com>
2025-11-09 14:11:20 +1100

    Debugging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/LimeComp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/LimeComp.java
@@ -120,7 +120,7 @@ public class LimeComp extends LinearOpMode {
 
     limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
     limelight.start(); // This tells Limelight to start looking!
-    limelight.pipelineSwitch(0); // Switch to pipeline number 0
+    limelight.pipelineSwitch(1); // Switch to pipeline number 0
 
     LLResult result = limelight.getLatestResult();
 
