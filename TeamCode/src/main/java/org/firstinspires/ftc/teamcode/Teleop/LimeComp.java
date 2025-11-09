c5458e6badaec6bfbd14507db524bcfc3fc16920
Jadzia-P <cheeky4pratt@gmail.com>
2025-11-09 17:47:06 +1100

    Debugging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/LimeComp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/LimeComp.java
@@ -318,14 +318,17 @@ public class LimeComp extends LinearOpMode {
         List<FiducialResult> fiducials = result.getFiducialResults();
         for (FiducialResult fiducial : fiducials) {
           int id = fiducial.getFiducialId(); // The ID number of the fiducial
-          double x = fiducial.getRobotPoseTargetSpace().getPosition().x; // Horizontal offset (meters)
-          double y = fiducial.getRobotPoseTargetSpace().getPosition().y; // Vertical offset (meters)
-          double z = fiducial.getRobotPoseTargetSpace().getPosition().z; // Forward distance (meters)
-          double StrafeDistance_3D = fiducial.getRobotPoseTargetSpace().getPosition().y;
-          double distance = Math.sqrt((x * x) + (y * y)); // Use horizontal plane distance to the tag
-          telemetry.addData("Fiducial " + id, "is " + distance + " meters away");
 
-          ShooterTarget = Constants.ShooterCal.interpolate(distance);
+          if (id == 20) {
+            double x = fiducial.getRobotPoseTargetSpace().getPosition().x; // Horizontal offset (meters)
+            double y = fiducial.getRobotPoseTargetSpace().getPosition().y; // Vertical offset (meters)
+            double z = fiducial.getRobotPoseTargetSpace().getPosition().z; // Forward distance (meters)
+            double StrafeDistance_3D = fiducial.getRobotPoseTargetSpace().getPosition().y;
+            double distance = Math.sqrt((x * x) + (y * y)); // Use horizontal plane distance to the tag
+            telemetry.addData("Fiducial " + id, "is " + distance + " meters away");
+
+            ShooterTarget = Constants.ShooterCal.interpolate(distance);
+          }
         }
       }
       else
